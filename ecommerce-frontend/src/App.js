acafdf770f08fa9d84535209c8755916f4f3c536
aliahshakirin <47250708+aliahshakirin@users.noreply.github.com>
2022-08-26 22:38:39 +0800

    Testing api call works using hello endpoint

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    fetch("/hello")
+     .then((response) => console.log(response.json()));
+   }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My ecommerce website</h1>
     </div>
   );
 }
